5d803f11d3098d21977972dade10427191518462
nddat1811 <63857350+nddat1811@users.noreply.github.com>
2024-03-07 20:51:18 +0700

    api get detail board

--- src/routes/v1/board.route.js
+++ src/routes/v1/board.route.js
@@ -5,12 +5,8 @@ import { boardController } from '~/controllers/board.controller'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.status(StatusCodes.OK).send('Hello World! board')
-})
-router.get('/v2', (req, res) => {
-  res.send('test')
-})
 router.post('/', boardValidation.createNew, boardController.createNew)
+
+router.route('/:id').get(boardController.getDetails).put()
 
 export const boardRouter = router
--- src/services/board.service.js
+++ src/services/board.service.js
@@ -1,8 +1,8 @@
 /*eslint-disable no-useless-catch */
-
-import { StatusCodes } from 'http-status-codes'
-import ApiError from '~/utils/ApiError'
 import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/board.model'
+import ApiError from '~/utils/ApiError'
+import { StatusCodes } from 'http-status-codes'
 
 const createNew = async reqBody => {
   try {
@@ -10,12 +10,30 @@ const createNew = async reqBody => {
       ...reqBody,
       slug: slugify(reqBody.title)
     }
-    return newBoard
+
+    const createdBoard = await boardModel.createNew(newBoard)
+
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+    return getNewBoard
+  } catch (error) {
+    throw error
+  }
+}
+
+const getDetails = async id => {
+  try {
+    console.log(id);
+    const res = await boardModel.getDetails(id)
+    if (!res) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found')
+    }
+    return res
   } catch (error) {
     throw error
   }
 }
 
 export const boardService = {
-  createNew
+  createNew,
+  getDetails
 }
